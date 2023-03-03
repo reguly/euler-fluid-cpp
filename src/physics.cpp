5158c7718f79e79507f823cdfcc2625006a858c5
Istvan Reguly <regulyistvan@gmail.com>
2023-03-03 15:16:33 +0100

    reindent

--- src/physics.cpp
+++ src/physics.cpp
@@ -10,50 +10,49 @@ Physics::Physics() {}
 Physics::~Physics() {}
 
 void Physics::SetBnd(int b, float x[], int N) {
-        for(int i = 1; i < N - 1; i++) {
-            	x[IX(i, 0, N)] = b == 2 ? -x[IX(i, 1, N)] : x[IX(i, 1, N)];
-            	x[IX(i, N-1, N)] = b == 2 ? -x[IX(i, N-2, N)] : x[IX(i, N-2, N)];
-        }
+	for(int i = 1; i < N - 1; i++) {
+			x[IX(i, 0, N)] = b == 2 ? -x[IX(i, 1, N)] : x[IX(i, 1, N)];
+			x[IX(i, N-1, N)] = b == 2 ? -x[IX(i, N-2, N)] : x[IX(i, N-2, N)];
+	}
+	for(int j = 1; j < N - 1; j++) {
+			x[IX(0, j, N)] = b == 1 ? -x[IX(1, j, N)] : x[IX(1, j, N)];
+			x[IX(N-1, j, N)] = b == 1 ? -x[IX(N-2, j, N)] : x[IX(N-2, j, N)];
+	}
 
-        for(int j = 1; j < N - 1; j++) {
-            	x[IX(0, j, N)] = b == 1 ? -x[IX(1, j, N)] : x[IX(1, j, N)];
-            	x[IX(N-1, j, N)] = b == 1 ? -x[IX(N-2, j, N)] : x[IX(N-2, j, N)];
-        }
-    
-    	x[IX(0, 0, N)] = 0.33f * (x[IX(1, 0, N)]
-                                  + x[IX(0, 1, N)]
-                                  + x[IX(0, 0, N)]);
-    	x[IX(0, N-1, N)] = 0.33f * (x[IX(1, N-1, N)]
-                                  + x[IX(0, N-2, N)]
-                                  + x[IX(0, N-1, N)]);
-    	x[IX(N-1, 0, N)] = 0.33f * (x[IX(N-2, 0, N)]
-                                  + x[IX(N-1, 1, N)]
-                                  + x[IX(N-1, 0, N)]);
-    	x[IX(N-1, N-1, N)] = 0.33f * (x[IX(N-2, N-1, N)]
-                                  + x[IX(N-1, N-2, N)]
-                                  + x[IX(N-1, N-1, N)]);
+	x[IX(0, 0, N)] = 0.33f * (x[IX(1, 0, N)]
+								+ x[IX(0, 1, N)]
+								+ x[IX(0, 0, N)]);
+	x[IX(0, N-1, N)] = 0.33f * (x[IX(1, N-1, N)]
+								+ x[IX(0, N-2, N)]
+								+ x[IX(0, N-1, N)]);
+	x[IX(N-1, 0, N)] = 0.33f * (x[IX(N-2, 0, N)]
+								+ x[IX(N-1, 1, N)]
+								+ x[IX(N-1, 0, N)]);
+	x[IX(N-1, N-1, N)] = 0.33f * (x[IX(N-2, N-1, N)]
+								+ x[IX(N-1, N-2, N)]
+								+ x[IX(N-1, N-1, N)]);
 }
 
 void Physics::LinSolve(int b, float x[], float x0[], float a, float c, int iter, int N, float temp[]) {
 	float cRecip = 1.0 / c;
-  float *_x = x;
-  float *_tmp = temp;
-  for (int k = 0; k < iter; k++) {
-    for (int j = 1; j < N - 1; j++) {
-      for (int i = 1; i < N - 1; i++) {
-        _tmp[IX(i, j, N)] = (x0[IX(i, j, N)] + a 
-            *(_x[IX(i+1, j, N)]
-              +_x[IX(i-1, j, N)]
-              +_x[IX(i, j+1, N)]
-              +_x[IX(i, j-1, N)]
-              +_x[IX(i, j, N)]
-              +_x[IX(i, j, N)]
-             )) * cRecip;
-                	}
-    }
-    std::swap(_x, _tmp);
-    this->SetBnd(b, _x, N);
-  }
+	float *_x = x;
+	float *_tmp = temp;
+	for (int k = 0; k < iter; k++) {
+	for (int j = 1; j < N - 1; j++) {
+		for (int i = 1; i < N - 1; i++) {
+		_tmp[IX(i, j, N)] = (x0[IX(i, j, N)] + a 
+			*(_x[IX(i+1, j, N)]
+				+_x[IX(i-1, j, N)]
+				+_x[IX(i, j+1, N)]
+				+_x[IX(i, j-1, N)]
+				+_x[IX(i, j, N)]
+				+_x[IX(i, j, N)]
+				)) * cRecip;
+					}
+	}
+	std::swap(_x, _tmp);
+	this->SetBnd(b, _x, N);
+	}
 }
 
 void Physics::Diffuse(int b, float x[], float x0[], float diff, float dt, int iter, int N, float temp[]) {
@@ -62,17 +61,17 @@ void Physics::Diffuse(int b, float x[], float x0[], float diff, float dt, int it
 }
 
 float Physics::Project(float vx[], float vy[], float p[], float div[], int iter, int N, float temp[]) {
-        for (int j = 1; j < N - 1; j++) {
-            	for (int i = 1; i < N - 1; i++) {
-                	div[IX(i, j, N)] = -0.5f*(
-                         	 vx[IX(i+1, j, N)]
-	                        -vx[IX(i-1, j, N)]
-	                	+vy[IX(i, j+1, N)]
-                        	-vy[IX(i, j-1, N)]
-                    	)/N;
-                	p[IX(i, j, N)] = 0;
-            	}
-        }
+	for (int j = 1; j < N - 1; j++) {
+		for (int i = 1; i < N - 1; i++) {
+			div[IX(i, j, N)] = -0.5f*(
+						vx[IX(i+1, j, N)]
+					-vx[IX(i-1, j, N)]
+				+vy[IX(i, j+1, N)]
+					-vy[IX(i, j-1, N)]
+				)/N;
+			p[IX(i, j, N)] = 0;
+		}
+	}
 
 	this->SetBnd(0, div, N); 
 	this->SetBnd(0, p, N);
@@ -94,48 +93,45 @@ float Physics::Project(float vx[], float vy[], float p[], float div[], int iter,
 void Physics::Advect(int b, float d[], float d0[], float vx[], float vy[], float dt, int N) {
 	float i0, i1, j0, j1;
     
-    	float dtx = dt * (N - 2);
-    	float dty = dt * (N - 2);
-    
-    	float s0, s1, t0, t1;
-    	float tmp1, tmp2, x, y;
-    
-    	float Nfloat = N;
-    	float ifloat, jfloat;
+	float dtx = dt * (N - 2);
+	float dty = dt * (N - 2);
 
-    	int i, j;
-    
-	for(j = 1, jfloat = 1; j < N - 1; j++, jfloat++) { 
-		for(i = 1, ifloat = 1; i < N - 1; i++, ifloat++) {
-                	tmp1 = dtx * vx[IX(i, j, N)];
-                	tmp2 = dty * vy[IX(i, j, N)];
-                	x = ifloat - tmp1; 
-                	y = jfloat - tmp2;
-                
-                	if(x < 0.5f) x = 0.5f; 
-                	if(x > Nfloat + 0.5f) x = Nfloat + 0.5f; 
-                	i0 = ::floorf(x); 
-                	i1 = i0 + 1.0f;
-                	if(y < 0.5f) y = 0.5f; 
-                	if(y > Nfloat + 0.5f) y = Nfloat + 0.5f; 
-                	j0 = ::floorf(y);
-                	j1 = j0 + 1.0f; 
+	float s0, s1, t0, t1;
+	float tmp1, tmp2, x, y;
 
-                	s1 = x - i0; 
-                	s0 = 1.0f - s1; 
-               		t1 = y - j0; 
-                	t0 = 1.0f - t1;
-                
-                	int i0i = i0;
-                	int i1i = i1;
-                	int j0i = j0;
-                	int j1i = j1;
+	float Nfloat = N;
+    
+	for(int j = 1; j < N - 1; j++) { 
+		for(int i = 1; i < N - 1; i++) {
+			tmp1 = dtx * vx[IX(i, j, N)];
+			tmp2 = dty * vy[IX(i, j, N)];
+			x = float(i) - tmp1; 
+			y = float(j) - tmp2;
+		
+			if(x < 0.5f) x = 0.5f; 
+			if(x > Nfloat + 0.5f) x = Nfloat + 0.5f; 
+			i0 = ::floorf(x); 
+			i1 = i0 + 1.0f;
+			if(y < 0.5f) y = 0.5f; 
+			if(y > Nfloat + 0.5f) y = Nfloat + 0.5f; 
+			j0 = ::floorf(y);
+			j1 = j0 + 1.0f; 
+
+			s1 = x - i0; 
+			s0 = 1.0f - s1; 
+			t1 = y - j0; 
+			t0 = 1.0f - t1;
+		
+			int i0i = i0;
+			int i1i = i1;
+			int j0i = j0;
+			int j1i = j1;
                 
 			d[IX(i, j, N)] = 
 				s0 * (t0 * d0[IX(i0i, j0i, N)] + t1 * d0[IX(i0i, j1i, N)]) +
 				s1 * (t0 * d0[IX(i1i, j0i, N)] + t1 * d0[IX(i1i, j1i, N)]);
-            	}
-        }
+		}
+	}
 	this->SetBnd(b, d, N);
 }
 
